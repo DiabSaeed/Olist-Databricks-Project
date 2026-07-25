7dddd1dc116f5338bd1e0d515531b5c9af19ecd6
diabsaeed7@gmail.com <diabsaeed7@gmail.com>
2026-07-25 11:27:52 +0000

    Adding __init__.py file and changing the location of Config.py

--- utils/Config.py
+++ utils/Config.py
@@ -9,11 +9,7 @@ class Config:
     RAW_VOLUME = "raw_csvs"
 
     RAW_PATH = f"/Volumes/{CATALOG}/{RAW_SCHEMA}/{RAW_VOLUME}"
-    FILE_NAMES = dbutils.fs.ls(RAW_PATH)
     BRONZE_DATABASE = f"{CATALOG}.{BRONZE_SCHEMA}"
     SILVER_DATABASE = f"{CATALOG}.{SILVER_SCHEMA}"
     GOLD_DATABASE = f"{CATALOG}.{GOLD_SCHEMA}"
-    DATASET = {
-        path.name.split("_")[1]: path.name for path in FILE_NAMES
-    }
-
+    
